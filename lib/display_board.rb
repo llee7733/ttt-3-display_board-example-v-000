4574bebffe3ae97c5a8f649b91eb82ae7058024b
Lloyd Lee <kinglloydlee@gmail.com>
2018-06-20 19:51:12 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,9 +2,7 @@
 def display_board(
   board = ["   ", "   ", "   ", "   ", "   ", "   ", "   ", "   ", "   ")
   #board output
-puts " #{board[0]} | #{board[1]} | #{board[2]} "
 puts "-----------"
-puts " #{board[3]} | #{board[4]} | #{board[5]} "
 puts "-----------"
-puts " #{board[6]} | #{board[7]} | #{board[8]} "
+
 end
